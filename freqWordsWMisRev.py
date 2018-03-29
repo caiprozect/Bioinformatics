a3cda3ef8bb4e773f4e6b116ff8a23ad0016c5ed
unknown <caiprozect@gmail.com>
2018-03-29 10:19:40 +0900

    Usual update of progress

--- freqWordsWMisRev.py
+++ freqWordsWMisRev.py
@@ -2,13 +2,31 @@ from neighbors import *
 from cptFreq import *
 from collections import Counter
 
+def revComplement(sPtn):
+	nLen = len(sPtn)
+	sRevC = ""
+	for i in range(nLen):
+		nucl = ''
+		if sPtn[i] == 'A':
+			nucl = 'T'
+		elif sPtn[i] == 'C':
+			nucl = 'G'
+		elif sPtn[i] == 'G':
+			nucl = 'C'
+		elif sPtn[i] == 'T':
+			nucl = 'A'
+		sRevC = nucl + sRevC
+	return sRevC
+
 def freqWordsWMismatches(sText, nLenPoly, nDist):
 	lWordsWMismatches = []
 	nLenText = len(sText)
 	for i in range(nLenText - nLenPoly + 1):
 		sPtn = sText[i:i+nLenPoly]
+		sRevC = revComplement(sPtn)
 		lPtnWMis = neighbors(sPtn, nDist)
-		lWordsWMismatches += [ptnToNumb(ptn) for ptn in lPtnWMis]
+		lRCWMis = neighbors(sRevC, nDist)
+		lWordsWMismatches += [ptnToNumb(ptn) for ptn in (lPtnWMis+lRCWMis)]
 	freqNumbWMis = Counter(lWordsWMismatches)
 	freqVals = np.array(list(freqNumbWMis.values()))
 	freqKeys = np.array(list(freqNumbWMis.keys()))
